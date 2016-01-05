addc2de27c10729a94035d096570765fccfe8cd5
James Hiscock <james@small.mu>
2016-01-06 00:17:42 +1100

    Related fields!!!

--- apps/api-server/web/graphql/recurring.ex
+++ apps/api-server/web/graphql/recurring.ex
@@ -1,6 +1,8 @@
 defmodule BudgetApi.GraphQL.Recurring do
   import Ecto.Query
   alias GraphQL.ObjectType
+  alias GraphQL.List
+  alias BudgetApi.GraphQL.Tag
 
   def schema do
     %ObjectType{
@@ -10,28 +12,48 @@ defmodule BudgetApi.GraphQL.Recurring do
         id: %{ type: "ID" },
         frequency: %{ type: "String" },
         amount: %{ type: "Float" },
-        description: %{ type: "String" }
+        description: %{ type: "String" },
+        tags: %{ type: %List{ of_type: Tag.schema } }
       }
     }
   end
 
   def resolve(_, %{id: id}, _) do
-    BudgetApi.Repo.get!(BudgetApi.Recurring, id)
-    |> serialise
+    query = from r in BudgetApi.Recurring,
+      where: r.id == ^id,
+      preload: :tags
+
+    [recurring] = BudgetApi.Repo.all(query)
+    serialise(recurring)
   end
 
   def resolve_list(_, %{offset: offset, limit: limit}, _) do
     query = from t in BudgetApi.Recurring,
       offset: ^offset,
-      limit: ^limit
+      limit: ^limit,
+      preload: :tags
 
-    query |> BudgetApi.Repo.all |> Enum.map(&serialise/1)
+    query
+    |> BudgetApi.Repo.all
+    |> Enum.map(&serialise/1)
   end
   def resolve_list(_, %{offset: offset}, _), do: resolve_list(%{}, %{offset: offset, limit: 10}, %{})
   def resolve_list(_, %{limit: limit}, _), do: resolve_list(%{}, %{offset: 0, limit: limit}, %{})
   def resolve_list(_, _, _), do: resolve_list(%{}, %{offset: 0, limit: 10}, %{})
 
   defp serialise(model) do
-    Map.take(model, [:id, :frequency, :amount, :description])
+    serialised = model
+    |> Map.take([:id, :frequency, :amount, :description, :tags])
+    |> Map.update!(:tags, &serialise_tags/1)
+
+    serialised
+  end
+
+  defp serialise_tags(tags) do
+    Enum.map(tags, &serialise_tag/1)
+  end
+
+  defp serialise_tag(model) do
+    Map.take(model, [:id, :tag])
   end
 end

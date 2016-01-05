e127a3bd718fcbaaee40674c1eebaf0429bb4aaf
James Hiscock <james@small.mu>
2016-01-06 00:15:02 +1100

    Primary keys don't exist for these tables

--- apps/api-server/web/models/recurring_tag.ex
+++ apps/api-server/web/models/recurring_tag.ex
@@ -1,9 +1,11 @@
 defmodule BudgetApi.RecurringTag do
   use BudgetApi.Web, :model
 
+  @primary_key false
+
   schema "recurrings_tags" do
-    belongs_to :recurring, BudgetApi.Recurring
-    belongs_to :tag, BudgetApi.Tag
+    belongs_to :recurring, BudgetApi.Recurring, references: :id
+    belongs_to :tag, BudgetApi.Tag, references: :id
   end
 
   @required_fields ~w(recurring_id tag_id)
--- apps/api-server/web/models/transaction_tag.ex
+++ apps/api-server/web/models/transaction_tag.ex
@@ -1,9 +1,11 @@
 defmodule BudgetApi.TransactionTag do
   use BudgetApi.Web, :model
 
+  @primary_key false
+
   schema "transactions_tags" do
-    belongs_to :transaction, BudgetApi.Transaction
-    belongs_to :tag, BudgetApi.Tag
+    belongs_to :transaction, BudgetApi.Transaction, references: :id
+    belongs_to :tag, BudgetApi.Tag, references: :id
   end
 
   @required_fields ~w(transaction_id tag_id)
